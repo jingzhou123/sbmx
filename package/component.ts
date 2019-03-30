1efc93a9f4a9774fc678b538df426e9030b7b65c
test <test@example.com>
2019-03-31 00:14:02 +0800

    add component command

--- package/component.ts
+++ package/component.ts
@@ -1,17 +1,19 @@
 import * as fs from 'fs'
 import * as _ from 'lodash'
 
-function getFileContext(apiName: string) {
-    return 'export class ' + apiName + 'Api {\n' +
-           '\tapi1() {\n' +
+function getFileContext(componentName: string) {
+    return 'import React, { Component } from "react"\n\n' +
+           'export class ' + componentName + ' {\n' +
+           '\trender() {\n' +
+           '\t\treturn <div></div>\n' +
            '\t}\n' +
            '}'
 }
-export function api(name: string) {
+export function component(name: string) {
     var camel = _.camelCase(name)
     var kebab = _.kebabCase(name) // foo-bar
     var upper = _.upperFirst(camel)
-    var filePath = kebab + '-api.ts'
+    var filePath = kebab + '-component.tsx'
     if (fs.existsSync(filePath)) {
         console.warn('file existed')
         process.exit(0)
--- package/index.ts
+++ package/index.ts
@@ -1,10 +1,19 @@
 import * as program from 'commander'
 import { api } from './api';
+import { component } from './component';
 const pkg = require('../package.json')
 
 program
   .version(pkg.version)
+
+program
   .command('api <name>')
-  .action(api);
+  .description('generate api')
+  .action(api)
+
+program
+  .command('component <name>')
+  .description('generate react component')
+  .action(component)
 
 program.parse(process.argv);
